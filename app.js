4e3f3351b25884b2eb9980d1eaab9b349a2985e7
C.BUTET <clement.butet@orange.fr>
2023-01-29 23:09:36 -0500

    add correcting

--- app.js
+++ app.js
@@ -112,4 +112,9 @@ mongoose
 
 // Start server
 httpProxy.createProxyServer({target:'https://avenuemochaseculavalweb.herokuapp.com'}).listen(8000);
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
+  res.end();
+}).listen(9000);
 console.log(`Server running on https://localhost:${process.env.PORT}`);
