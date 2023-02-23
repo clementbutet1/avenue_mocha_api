6201c7f10e81fbdb73bdb76e50ae30105b83eeac
C.BUTET <clement.butet@orange.fr>
2023-02-23 11:03:28 -0500

    add correcting

--- controllers/users.js
+++ controllers/users.js
@@ -50,7 +50,7 @@ const getToken = async (user, res) => {
     { expiresIn: "1d" }
   );
   res
-  .cookie('token', token, {httpOnly: true, secure: true, sameSite: 'strict', domain: process.env.URL_FRONT, path: '/'} )
+  .cookie('token', token, {httpOnly: true, secure: true, domain: process.env.URL_FRONT, path: '/'} )
   .json({
         message: "Auth successful",
         user,
