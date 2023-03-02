356b350b71e27d3dddb3fd2d2ea3f8e5fdbdaba2
C.BUTET <clement.butet@orange.fr>
2023-03-02 16:42:36 -0500

    add correcting to the expiration date of cookiies

--- app.js
+++ app.js
@@ -85,7 +85,6 @@ app.use(
       // sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
       // secure: ((process.env.NODE_ENV === "development") ? false : true),
       httpOnly: true,
-      expires: new Date(Date.now() + 5184000000),
     },
   })
 );
